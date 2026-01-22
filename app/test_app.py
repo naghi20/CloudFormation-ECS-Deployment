9a18fb0c0ab096da5da41b0575736da0bc219566
Ahmed AL Naghi <64667680+naghi20@users.noreply.github.com>
2026-01-22 21:18:15 +0300

    Add test for home route in test_app.py

---
+++ app/test_app.py
@@ -0,0 +1,8 @@
+# app/test_app.py
+from app import app
+
+def test_home():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert response.status_code == 200
+    assert b"Automated CICD_ECR Deployment Working!" in response.data
